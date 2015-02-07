0e03baae9c0a2ed2a99dec0631e3029893fe5d07
R2 D2 <p234a137@gmail.com>
2015-02-06 16:09:30 -0800

    finish first plot, start second

---
+++ plot2.R
@@ -0,0 +1,27 @@
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+
+## read in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+# The overall goal of this assignment is to explore the National Emissions Inventory database
+# and see what it say about fine particulate matter pollution in the United states over the 10-year
+# period 1999–2008. You may use any R package you want to support your analysis.
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all
+# sources for each of the years 1999, 2002, 2005, and 2008.
+
+library('dplyr')
+emissions_vs_year <- NEI %>%
+  group_by(year) %>%
+  summarize(sum(Emissions))
+
+# cross-check
+# sum(NEI[NEI$year == '1999','Emissions'])
+
+#png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(emissions_vs_year$'sum(Emissions)' ~ emissions_vs_year$year, type='l', lty = 1, lwd = 5, col = 4,
+     xlab = 'Year', ylab = 'Emissions', main = 'Emissions vs. Year')
+#dev.off()
