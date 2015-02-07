11ca5a57f670f4496b8715dc3be55bb5b4fd113c
R2 D2 <p234a137@gmail.com>
2015-02-06 21:29:56 -0800

    filtering data for Baltimore

--- plot2.R
+++ plot2.R
@@ -4,17 +4,12 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-
-# The overall goal of this assignment is to explore the National Emissions Inventory database
-# and see what it say about fine particulate matter pollution in the United states over the 10-year
-# period 1999–2008. You may use any R package you want to support your analysis.
-
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all
-# sources for each of the years 1999, 2002, 2005, and 2008.
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510")
+# from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
 library('dplyr')
-emissions_vs_year <- NEI %>%
+emissions_vs_year_baltimore <- NEI %>%
+  filter( fips == "24510") %>%
   group_by(year) %>%
   summarize(sum(Emissions))
 
@@ -22,6 +17,6 @@ emissions_vs_year <- NEI %>%
 # sum(NEI[NEI$year == '1999','Emissions'])
 
 #png(filename = "plot2.png", width = 480, height = 480, units = "px")
-plot(emissions_vs_year$'sum(Emissions)' ~ emissions_vs_year$year, type='l', lty = 1, lwd = 5, col = 4,
-     xlab = 'Year', ylab = 'Emissions', main = 'Emissions vs. Year')
+plot(emissions_vs_year_baltimore$'sum(Emissions)' ~ emissions_vs_year_baltimore$year, type='l', lty = 1, lwd = 5, col = 4,
+     xlab = 'Year', ylab = 'Emissions', main = 'Emissions vs. Year in Baltimore')
 #dev.off()
